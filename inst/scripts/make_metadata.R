df80320b63770bab8837d0e62c288b76662221c9
tram-nguyen-n <tram_nguyen@hms.harvard.edu>
2024-07-23 01:33:21 +0000

    add new file names to .rds

--- inst/scripts/make_metadata.R
+++ inst/scripts/make_metadata.R
@@ -5,29 +5,29 @@
 
 # AlphaMissense_Supplementary
 AlphaMissense_Supplementary <- data.frame(
-    Title = "Cheng_ProteinGym_S8_supplementary",
-    Description = paste("ProteinGym data provided by Supplementary Table S9",
-                         "from Cheng et al. 2023"),
+    Title = "ProteinGym Data from the AlphaMissense publication",
+    Description = paste("ProteinGym variants from the AlphaMissense",
+                        "table from Cheng et al. 2023"),
     BiocVersion = "3.20",
     Genome = "hg38",
     SourceType = "RDS",
     SourceUrl = "https://www.science.org/doi/10.1126/science.adg7492",
     SourceVersion = NA,
-    Species = "Homo Sapiens",
+    Species = "Homo sapiens",
     TaxonomyId = 9606,
     Coordinate_1_based = NA,
     DataProvider = "Cheng et al. 2023",
     Maintainer = "Tram Nguyen <Tram_Nguyen@hms.harvard.edu>",
     RDataClass = "Data.Frame",
     DispatchClass = "RDS",
-    RDataPath = "ProteinGymR/Cheng_ProteinGym_S8_supplementary",
+    RDataPath = "ProteinGymR/Cheng_ProteinGym_variants.rds",
     stringsAsFactors = FALSE
 )
 
 
 # ProteinGym_DMS_substitutions_v1
 ProteinGym_DMS_subs <- data.frame(
-    Title = "ProteinGym_DMS_substitutions_v1",
+    Title = "ProteinGym DMS Information for Substitutions",
     Description = paste0("ProteinGym DMS information for 216 assays",
                          "from Notin et al. 2023"),
     BiocVersion = "3.20",
@@ -35,14 +35,14 @@ ProteinGym_DMS_subs <- data.frame(
     SourceType = "RDS",
     SourceUrl = "https://proteingym.org/",
     SourceVersion = NA,
-    Species = "Homo Sapiens",
+    Species = "all",
     TaxonomyId = 9606,
     Coordinate_1_based = NA,
     DataProvider = "Marks Lab at Harvard Medical School",
     Maintainer = "Tram Nguyen <Tram_Nguyen@hms.harvard.edu>",
     RDataClass = "List",
     DispatchClass = "RDS",
-    RDataPath = "ProteinGymR/ProteinGym_DMS_substitutions_v1",
+    RDataPath = "ProteinGymR/ProGym216_DMS_subs_v1.rds",
     stringsAsFactors = FALSE
 )
 
