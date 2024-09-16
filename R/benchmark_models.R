fdbf5f038b87894a5c2116281634161214873957
tram-nguyen-n <tram_nguyen@hms.harvard.edu>
2024-09-16 17:32:12 +0000

    add `check_metric_argument`

--- R/benchmark_models.R
+++ R/benchmark_models.R
@@ -1,15 +1,58 @@
 #' @rdname benchmark_models
 #' 
 #' @noRd
-# Model metric argument check
+# Metric argument check
+check_metric_argument <- function(metric){
+    
+    # Check whether models specified or NULL
+    if (is.null(metric)) {
+        cat("No metric specified. Using default Spearman correlation. \n")
+    } else {
+    
+        # Check if provided models are valid
+        valid_metrics <- c("AUC", "MCC", "NDCG", "Spearman", "Top_recall")
+        
+        if (!all(metric %in% valid_metrics)) {
+            
+            invalid_metric <- metric[!metric %in% valid_metrics]
+            
+            stop(paste("Invalid model(s) specified:", 
+            paste(invalid_metric, collapse = ", ")))
+        }
+    
+        # Check that only metric passed
+        if (length(metric) > 1) {
+            stop("Select only one metric for comparison.")
+        }
+    }
+}
+#'    
+#' @noRd
+# Models argument check
 check_model_argument <- function(models){
     
     # Check whether models specified or NULL
     if (is.null(models)) {
-        cat("No models specified. Using default models.")
+        cat("No models specified. Using default models. \n")
     } else {
     
-        # Check if provided models are valid
+    # Check if provided models are valid
+    valid_models <- c("Site_Independent", "EVmutation", "DeepSequence_single",
+        "DeepSequence_ensemble", "EVE_single", "EVE_ensemble", "Unirep", 
+        "Unirep_evotuned", "MSA_Transformer_single", "MSA_Transformer_ensemble",
+        "ESM_1b",  "ESM_1v_single", "ESM_1v_ensemble", "ESM2_8M", "ESM2_35M", 
+        "ESM2_150M", "ESM2_650M", "ESM2_3B", "ESM2_15B", "Wavenet", 
+        "RITA_S", "RITA_M", "RITA_L", "RITA_XL", "Progen2_S", "Progen2_M",
+        "Progen2_Base", "Progen2_L", "Progen2_XL", "GEMME", "VESPA", "VESPAl", 
+        "ProtGPT2", "Tranception_S_no_retrieval", "Tranception_M_no_retrieval", 
+        "Tranception_L_no_retrieval", "Tranception_S", "Tranception_M", 
+        "Tranception_L", "TranceptEVE_S", "TranceptEVE_M",  "TranceptEVE_L", 
+        "CARP_38M", "CARP_600K", "CARP_640M", "CARP_76M", "MIF", "MIF_ST",
+        "ESM_IF1", "ProteinMPNN", "ProtSSN_k_10_h_512", "ProtSSN_k_10_h_768", 
+        "ProtSSN_k_10_h_1280", "ProtSSN_k_20_h_512", "ProtSSN_k_20_h_768", 
+        "ProtSSN_k_20_h_1280", "ProtSSN_k_30_h_512", "ProtSSN_k_30_h_768", 
+        "ProtSSN_k_30_h_1280", "ProtSSN_ensemble", "SaProt_650M", "SaProt_35M")
+    
         if (!all(models %in% valid_models)) {
             
             invalid_models <- models[!models %in% valid_models]
@@ -24,7 +67,6 @@ check_model_argument <- function(models){
         }
     }
 }
-#' 
 #' @title Benchmark effect prediction models
 #' 
 #' @description `benchmark_models()` plots one of the five model performance 
@@ -44,8 +86,8 @@ check_model_argument <- function(models){
 #'
 #' @examples
 #' 
-#' benchmark_models(metric = "Spearman", models = c("AlphaMissense", "EVE", 
-#' "DeepSequence_single")
+#' benchmark_models(metric = "Spearman", models = c("Site_Independent", 
+#' "DeepSequence_single", "ESM2_15B", "GEMME", "CARP_640M"))
 #' 
 #' @references Notin, P., Kollasch, A., Ritter, D., van Niekerk, L., Paul, S., 
 #' Spinner, H., Rollins, N., Shaw, A., Orenbuch, R., Weitzman, R., Frazer, J., 
@@ -65,24 +107,8 @@ benchmark_models <- function(
 ){
     
     # Check valid arguments
-    valid_metric <- c("AUC", "MCC", "NDCG", "Spearman", "Top_recall")
-    valid_models <- c("Site_Independent", "EVmutation", "DeepSequence_single",
-        "DeepSequence_ensemble", "EVE_single", "EVE_ensemble", "Unirep", 
-        "Unirep_evotuned", "MSA_Transformer_single", "MSA_Transformer_ensemble",
-        "ESM_1b",  "ESM_1v_single", "ESM_1v_ensemble", "ESM2_8M", "ESM2_35M", 
-        "ESM2_150M", "ESM2_650M", "ESM2_3B", "ESM2_15B", "Wavenet", 
-        "RITA_S", "RITA_M", "RITA_L", "RITA_XL", "Progen2_S", "Progen2_M",
-        "Progen2_Base", "Progen2_L", "Progen2_XL", "GEMME", "VESPA", "VESPAl", 
-        "ProtGPT2", "Tranception_S_no_retrieval", "Tranception_M_no_retrieval", 
-        "Tranception_L_no_retrieval", "Tranception_S", "Tranception_M", 
-        "Tranception_L", "TranceptEVE_S", "TranceptEVE_M",  "TranceptEVE_L", 
-        "CARP_38M", "CARP_600K", "CARP_640M", "CARP_76M", "MIF", "MIF_ST",
-        "ESM_IF1", "ProteinMPNN", "ProtSSN_k_10_h_512", "ProtSSN_k_10_h_768", 
-        "ProtSSN_k_10_h_1280", "ProtSSN_k_20_h_512", "ProtSSN_k_20_h_768", 
-        "ProtSSN_k_20_h_1280", "ProtSSN_k_30_h_512", "ProtSSN_k_30_h_768", 
-        "ProtSSN_k_30_h_1280", "ProtSSN_ensemble", "SaProt_650M", "SaProt_35M")
-    
     check_model_argument(models = models)
+    check_metric_argument(metric = metric)
     
     # Load in benchmark scores
     metric_tables <- zeroshot_DMS_metrics()
