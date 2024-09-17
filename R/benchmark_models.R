493ad77cca94efffacb44cf44cda847a10af7ed3
tram-nguyen-n <tram_nguyen@hms.harvard.edu>
2024-09-17 19:19:59 +0000

    - added `available_models()` - organized Imports - code to prep Spearman absolute values scores

--- R/benchmark_models.R
+++ R/benchmark_models.R
@@ -1,5 +1,24 @@
 #' @rdname benchmark_models
 #' 
+#' @export
+available_models <- function() 
+    c("Site_Independent", "EVmutation", "DeepSequence_single",
+        "DeepSequence_ensemble", "EVE_single", "EVE_ensemble", "Unirep", 
+        "Unirep_evotuned", "MSA_Transformer_single", "MSA_Transformer_ensemble",
+        "ESM_1b",  "ESM_1v_single", "ESM_1v_ensemble", "ESM2_8M", "ESM2_35M", 
+        "ESM2_150M", "ESM2_650M", "ESM2_3B", "ESM2_15B", "Wavenet", 
+        "RITA_S", "RITA_M", "RITA_L", "RITA_XL", "Progen2_S", "Progen2_M",
+        "Progen2_Base", "Progen2_L", "Progen2_XL", "GEMME", "VESPA", "VESPAl", 
+        "ProtGPT2", "Tranception_S_no_retrieval", "Tranception_M_no_retrieval", 
+        "Tranception_L_no_retrieval", "Tranception_S", "Tranception_M", 
+        "Tranception_L", "TranceptEVE_S", "TranceptEVE_M",  "TranceptEVE_L", 
+        "CARP_38M", "CARP_600K", "CARP_640M", "CARP_76M", "MIF", "MIF_ST",
+        "ESM_IF1", "ProteinMPNN", "ProtSSN_k_10_h_512", "ProtSSN_k_10_h_768", 
+        "ProtSSN_k_10_h_1280", "ProtSSN_k_20_h_512", "ProtSSN_k_20_h_768", 
+        "ProtSSN_k_20_h_1280", "ProtSSN_k_30_h_512", "ProtSSN_k_30_h_768", 
+        "ProtSSN_k_30_h_1280", "ProtSSN_ensemble", "SaProt_650M", "SaProt_35M")
+#'
+#'
 #' @noRd
 # Check metric argument
 check_metric_argument <- function(metric){
@@ -37,21 +56,7 @@ check_model_argument <- function(models){
     } else {
     
     # Check if provided models are valid
-    valid_models <- c("Site_Independent", "EVmutation", "DeepSequence_single",
-        "DeepSequence_ensemble", "EVE_single", "EVE_ensemble", "Unirep", 
-        "Unirep_evotuned", "MSA_Transformer_single", "MSA_Transformer_ensemble",
-        "ESM_1b",  "ESM_1v_single", "ESM_1v_ensemble", "ESM2_8M", "ESM2_35M", 
-        "ESM2_150M", "ESM2_650M", "ESM2_3B", "ESM2_15B", "Wavenet", 
-        "RITA_S", "RITA_M", "RITA_L", "RITA_XL", "Progen2_S", "Progen2_M",
-        "Progen2_Base", "Progen2_L", "Progen2_XL", "GEMME", "VESPA", "VESPAl", 
-        "ProtGPT2", "Tranception_S_no_retrieval", "Tranception_M_no_retrieval", 
-        "Tranception_L_no_retrieval", "Tranception_S", "Tranception_M", 
-        "Tranception_L", "TranceptEVE_S", "TranceptEVE_M",  "TranceptEVE_L", 
-        "CARP_38M", "CARP_600K", "CARP_640M", "CARP_76M", "MIF", "MIF_ST",
-        "ESM_IF1", "ProteinMPNN", "ProtSSN_k_10_h_512", "ProtSSN_k_10_h_768", 
-        "ProtSSN_k_10_h_1280", "ProtSSN_k_20_h_512", "ProtSSN_k_20_h_768", 
-        "ProtSSN_k_20_h_1280", "ProtSSN_k_30_h_512", "ProtSSN_k_30_h_768", 
-        "ProtSSN_k_30_h_1280", "ProtSSN_ensemble", "SaProt_650M", "SaProt_35M")
+    valid_models <- available_models()
     
         if (!all(models %in% valid_models)) {
             
@@ -67,33 +72,7 @@ check_model_argument <- function(models){
         }
     }
 }
-
-#'
-#' @noRd
-# Average model performance scores for plotting
-avg_model_scores <- function(DMS_table, metric){
-    
-    # Spearman -- Absolute value
-    if (metric == "Spearman"){ # -1 neg corr, 1 pos corr. Take abs value
-        
-        res <- DMS_table |> 
-            select(2:length(DMS_table)) 
-        res <- abs(res)
-        res <- cbind(DMS_table$DMS_ID, res)
-        colnames(res)[1] <- "DMS_ID"
-        
-    } else if (metric == "AUC"){ # 0 to 1
-        res <- DMS_table
-    } else if (metric == "MCC"){ # -1 worst; 1 best
-        res <- DMS_table
-    } else if (metric == "NDCG") { # 0 to 1
-        res <- DMS_table
-    } else # topk 0 to 1
-        res <- DMS_table
-    res
-}
-
-#'
+#' 
 #' @title Benchmark effect prediction models
 #' 
 #' @description `benchmark_models()` plots one of the five model performance 
@@ -101,17 +80,21 @@ avg_model_scores <- function(DMS_table, metric){
 #'    5 user-specified variant effect prediction tools. See reference for more
 #'    details about the metrics and models. 
 #'    
-#' @param metric `character()` the model performance metric to
-#'    compare ("AUC", "MCC", "NDCG", "Spearman", "Top_recall").
+#' @param metric `character()` a model performance metric to
+#'    benchmark ("AUC", "MCC", "NDCG", "Spearman", "Top_recall").
 #'    
-#' @param models `character()` a character vector listing up to five effect
-#'    prediction models to compare. If no model specified, the top five highest
-#'    performing models are displayed.
+#' @param models `character()` a character vector of up to five effect
+#'    prediction models to compare. Valid models can be seen with 
+#'    `available_models()`. If no models are specified, the five 
+#'    highest performing models (based on the mean score across 217 assays) 
+#'    are displayed.
 #'
 #' @return `benchmark_models()` returns a `ggplot` object visualizing a chosen
 #'    model performance metric between several variant effect prediction models.
 #'
 #' @examples
+#' # Currently support models
+#' available_models()
 #' 
 #' benchmark_models(metric = "Spearman", models = c("Site_Independent", 
 #' "DeepSequence_single", "ESM2_15B", "GEMME", "CARP_640M"))
@@ -123,22 +106,24 @@ avg_model_scores <- function(DMS_table, metric){
 #' Design. In A. Oh, T. Neumann, A. Globerson, K. Saenko, M. Hardt, & 
 #' S. Levine (Eds.), \emph{Advances in Neural Information Processing Systems} 
 #' (Vol. 36, pp. 64331-64379). Curran Associates, Inc.
-#' 
-#' @importFrom ggplot2 ggplot geom_bin2d aes element_text labs xlab ylab
-#'     scale_fill_continuous theme_classic annotate theme geom_boxplot
 #'     
-#' @importFrom dplyr select
+#' @importFrom dplyr select mutate group_by ungroup
 #' 
 #' @importFrom tidyr pivot_longer
 #' 
+#' @importFrom forcats fct_reorder
+#' 
+#' @importFrom ggplot2 ggplot coord_cartesian aes element_text scale_fill_discrete
+#'     theme_classic annotate theme geom_boxplot element_blank ylab
+#'     
 #' @importFrom ggdist stat_halfeye stat_dots
 #' 
 #' @importFrom gghalves geom_half_point
 #' 
 #' @export
 benchmark_models <- function(
-        metric = NULL, 
-        models = NULL) {
+    metric = c("AUC", "MCC", "NDCG", "Spearman", "Top_recall"), 
+    models = available_models()) {
     
     # Check valid arguments
     check_model_argument(models = models)
@@ -151,11 +136,15 @@ benchmark_models <- function(
     selected_table <- metric_tables[[metric]]
     selected_table <- selected_table |> select(all_of(c("DMS_ID", models)))
     
-    # Prepare scores for plotting
+    # If Spearman, take absolute value for plotting
     res <- selected_table |> 
-            select(2:length(selected_table)) 
+        select(2:length(selected_table)) 
     
-    res <- abs(res)
+    if (metric == "Spearman"){
+        res <- abs(res)
+    } else {
+        res
+    }
     
     res_long <- res |> 
         pivot_longer(cols = everything(), 
@@ -172,7 +161,7 @@ benchmark_models <- function(
     # Raincloud plot
     res_long |> 
         ggplot(aes(x = model, y = score, fill = model, group = model)) + 
-        ggdist::stat_halfeye(
+        stat_halfeye(
             adjust = .5, 
             width = .6, 
             .width = 0, 
@@ -183,39 +172,23 @@ benchmark_models <- function(
             width = .15, 
             outlier.shape = NA
         ) +
-        
-        # add justified jitter from the {gghalves} package
-        gghalves::geom_half_point(
+        geom_half_point(
             side = "l", 
             range_scale = .4, 
             alpha = .2
         ) +
-        
         # add theme and fonts
         coord_cartesian(clip = "off") +
-        theme_minimal() +
-        xlab("Models") +
-        ylab(paste(metric, "score")) +
+        scale_fill_discrete(name = "Models") +
         theme_classic() +
+        ylab(paste(metric, "score")) +
         theme(
             axis.text.x = element_text(size = 16),
             axis.text.y = element_text(size = 16),
             axis.title.y = element_text(size = 16),
-            axis.title.x = element_text(size = 16),
-            legend.title = element_blank(),
+            axis.title.x = element_blank(),
+            legend.title = element_text(size = 16),
             legend.text = element_text(size = 11)
-        ) 
-        
-        # # Add mean annotations
-        # geom_text(
-        #     data = order(unique(res_long$model_mean), decreasing = TRUE),
-        #     aes(y = mean_value, label = sprintf("Mean: %.2f", mean_value)),
-        #     vjust = -1,
-        #     size = 3.5,
-        #     fontface = "bold"
-        # ) +
-        # 
-        # # Customize the plot
-        # scale_fill_brewer(palette = "Set2")
+        )
 }
     
