bc546756421e7fca74c6ca4617eb192eef516caa
tram-nguyen-n <tram_nguyen@hms.harvard.edu>
2025-03-25 21:20:54 +0000

    add marginals to correlation plot

--- R/dms_corr_plot.R
+++ R/dms_corr_plot.R
@@ -242,6 +242,8 @@ pg_correlate <-
 #' 
 #' @importFrom ggplot2 ggplot geom_bin2d aes element_text labs xlab ylab
 #'     scale_fill_continuous theme_classic annotate theme
+#'     
+#' @importFrom ggExtra ggMarginal
 #' 
 #' @export
 dms_corr_plot <-
@@ -284,8 +286,9 @@ dms_corr_plot <-
             aes(y = .data$mean_am, x = .data$mean_dms)
         ) +
         geom_bin2d(bins = 60) +
+        geom_point(alpha = 0) +
         scale_fill_continuous(type = "viridis") +
-        labs(title = paste0("UniProt ID: ", uniprotId)) +
+        labs(title = paste0("\nUniProt ID: ", uniprotId)) +
         xlab("DMS score") +
         ylab("AlphaMissense score") +
         theme_classic() +
@@ -297,6 +300,16 @@ dms_corr_plot <-
             legend.title = element_text(size = 16),
             legend.text = element_text(size = 16)
         )
+    
+    # Add marginal density plots
+    pg_density_plot <- ggMarginal(
+        pg_density_plot,
+        type = "densigram", # Can also use "histogram"
+        fill = "#B0C4DE", 
+        color = "black"  # Change color as needed
+    )
+
+    pg_density_plot
     
     print(paste0("r = ", format(round(cor_results$estimate, 2)), 
                 "; Pval = ", cor_results$p.value))
