34eda019a1a085e075c546e63c958ae2656688c0
tram-nguyen-n <tram_nguyen@hms.harvard.edu>
2025-03-26 18:28:58 +0000

    metadata for all pdb files individually

--- inst/scripts/make_metadata.R
+++ inst/scripts/make_metadata.R
@@ -3,6 +3,8 @@
 ### -------------------------------------------------------------------------
 ###
 
+library(tools)
+
 # AlphaMissense_Supplementary
 AlphaMissense_Supplementary <- data.frame(
     Title = "AlphaMissense pathogenicity scores for variants in ProteinGym",
@@ -89,12 +91,63 @@ zeroshot_scores_DMS_subs <- data.frame(
     stringsAsFactors = FALSE
 )
 
+# Function to generate metadata for each PDB files 
+generate_pdb_metadata <- function(directory) {
+    # Get list of .pdb files in the directory
+    pdb_files <- list.files(directory, pattern = "\\.pdb$", full.names = FALSE)
+      
+    # Initialize an empty list to store metadata entries
+    metadata_list <- list()
+      
+    # Iterate through each .pdb file
+    for (file in pdb_files) {
+        # Remove .pdb extension for RDataPath
+        rdata_path <- paste0("ProteinGymR/", file_path_sans_ext(file), ".pdb")
+
+        # Create metadata entry
+        metadata_entry <- data.frame(
+            Title = paste("Protein structure for", file_path_sans_ext(file)),
+            Description = paste("AlphaFold2 predicted protein structure for", 
+                file_path_sans_ext(file), 
+                "from ProteinGym v1.2 curated by Notin et al. 2023"),
+            BiocVersion = "3.21",
+            Genome = NA,
+            SourceType = "PDB",
+            SourceUrl = "https://zenodo.org/records/14997691",
+            SourceVersion = NA,
+            Species = NA,
+            TaxonomyId = NA,
+            Coordinate_1_based = TRUE,
+            DataProvider = "Marks Lab at Harvard Medical School",
+            Maintainer = "Tram Nguyen <Tram_Nguyen@hms.harvard.edu>",
+            RDataClass = "Character",
+            DispatchClass = "FilePath",
+            RDataPath = rdata_path,
+            stringsAsFactors = FALSE
+        )
+        
+        # Add to list
+        metadata_list[[file]] <- metadata_entry
+    }
+    
+    # Combine all metadata entries into a single data frame
+    metadata_df <- do.call(rbind, metadata_list)
+    rownames(metadata_df) <- NULL
+      
+    return(metadata_df)
+}
+
+
+pdb_files <- generate_pdb_metadata("~/ProteinGym_data/ProteinGym_v1.2/ProteinGym_AF2_structures/")
+
+
 # Combined meta-data
 df_all <- base::rbind(
     AlphaMissense_Supplementary, 
     DMS_substitutions_scores,
     zeroshot_scores_DMS_subs,
-    DMS_sub_reference
+    DMS_sub_reference,
+    pdb_files
 )
 
 # Save .csv file
